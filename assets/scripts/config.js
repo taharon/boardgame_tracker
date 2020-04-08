80583faf20449747e8ce1283edbd490b12ebe4d7
taharon <taharon1990@gmail.com>
2020-04-08 07:35:18 -0400

    added heroku link to config

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://git.heroku.com/project-2-api-248.git',
   development: 'http://localhost:4741/'
 }
 
